07dc78682547dd57e33ec5608088ee0eb81fa115
Shih-Yi <brvast@gmail.com>
2023-04-17 11:08:34 +1200

    add skaffold setup config

--- posts/index.js
+++ posts/index.js
@@ -34,15 +34,13 @@ app.post("/posts/create", async (req, res) => {
   res.status(201).send(posts[id]);
 });
 
-app.post("/events/create", (req, res) => {
+app.post("/events", (req, res) => {
   console.log("Received Event", req.body.type);
 
   res.send({});
 });
 
 app.listen(4000, () => {
-  console.log("44555555555 .....");
-  console.log("7777777 .....");
-  console.log("8888888 .....");
+  console.log("8888888------");
   console.log("Listening on 4000");
 });
